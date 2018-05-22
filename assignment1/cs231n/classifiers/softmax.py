89a53768d6cf9549c3b9544d118290c3def1ea56
ckoiloth <koiloth2@illinois.edu>
2018-05-22 12:27:57 -0500

    cm

--- assignment1/cs231n/classifiers/softmax.py
+++ assignment1/cs231n/classifiers/softmax.py
@@ -29,11 +29,29 @@ def softmax_loss_naive(W, X, y, reg):
   # here, it is easy to run into numeric instability. Don't forget the        #
   # regularization!                                                           #
   #############################################################################
-  pass
+  num_classes = W.shape[1]
+  num_data = X.shape[0]
+  for i in range(0,num_data):
+    scores = X[i].dot(W)
+    scores = scores - np.max(scores)
+    false_prob = np.sum(np.exp(scores))
+    scores_p =np.exp(scores)/false_prob
+        
+    for j in range(0, num_classes):
+        if j != y[i]:
+            dW[:,j] += (np.exp(scores[j]) * X[i])/false_prob
+        else:
+            dW[:,j] += ((np.exp(scores[y[i]])*X[i])/false_prob) - X[i]
+            
+    
+    loss += -scores[y[i]] + np.log(false_prob)
   #############################################################################
   #                          END OF YOUR CODE                                 #
   #############################################################################
-
+  loss /= num_data
+  loss += reg * np.sum(W**2)
+  dW /= num_data
+  dW += 2*reg*W
   return loss, dW
 
 
@@ -53,7 +71,30 @@ def softmax_loss_vectorized(W, X, y, reg):
   # here, it is easy to run into numeric instability. Don't forget the        #
   # regularization!                                                           #
   #############################################################################
-  pass
+  scores = X.dot(W)
+  scores -= np.max(scores, axis=1, keepdims=True)
+  num_data = X.shape[0]
+  num_classes = W.shape[1]
+  test = scores[np.arange(num_data),:]
+  loss = np.log(np.sum(np.exp(scores[np.arange(num_data)]), axis = 1))-scores[np.arange(num_data), y]
+  loss = np.sum(loss)
+  
+  loss /= num_data
+  loss += reg * np.sum(W**2)  
+  
+  manipulated_scores = np.exp(scores)
+  manipulated_scores /= np.sum(manipulated_scores, axis = 1, keepdims = True)
+  #manipulated_scores[np.arange(num_data), y] -= 1
+  dW = np.dot(X.T,manipulated_scores)
+  
+  for i in range(num_data):
+      dW[:, y[i]] -= X[i].T
+  
+  dW /= num_data
+  dW += 2 * reg*W
+  #print(dW)
+  
+  
   #############################################################################
   #                          END OF YOUR CODE                                 #
   #############################################################################
